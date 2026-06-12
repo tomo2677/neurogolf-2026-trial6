677b039e10bdbba434d102003c8908aef9d88573
tomoki <tomoki.kawamoto2677@gmail.com>
2026-06-13 05:43:49 +0900

    Improve task031 index cost

--- solutions/task031.py
+++ solutions/task031.py
@@ -14,6 +14,11 @@ def _int64_tensor(name: str, values: list[int], dims: list[int] | None = None) -
     return helper.make_tensor(name, onnx.TensorProto.INT64, shape, values)
 
 
+def _int32_tensor(name: str, values: list[int], dims: list[int] | None = None) -> onnx.TensorProto:
+    shape = [len(values)] if dims is None else dims
+    return helper.make_tensor(name, onnx.TensorProto.INT32, shape, values)
+
+
 def _u8_tensor(name: str, values: list[int], dims: list[int]) -> onnx.TensorProto:
     return helper.make_tensor(name, onnx.TensorProto.UINT8, dims, values)
 
@@ -24,12 +29,13 @@ def build_model() -> onnx.ModelProto:
 
     initializers = [
         _int64_tensor("zero_i64", [0], [1]),
-        _int64_tensor("width_i64", [SIZE], [1]),
+        _int32_tensor("zero_i32", [0], [1]),
+        _int32_tensor("width_i32", [SIZE], [1]),
         _int64_tensor("shape_index_1x900", [1, 1, SIZE * SIZE], [3]),
         _int64_tensor("shape_flat_1x900", [1, 1, SIZE * SIZE], [3]),
         _int64_tensor("shape_1x1x30x30", [1, 1, SIZE, SIZE], [4]),
-        _int64_tensor("row_grid_i64", [r for r in range(SIZE) for _ in range(SIZE)], [1, 1, SIZE, SIZE]),
-        _int64_tensor("col_grid_i64", [c for _ in range(SIZE) for c in range(SIZE)], [1, 1, SIZE, SIZE]),
+        _int32_tensor("row_grid_i32", [r for r in range(SIZE) for _ in range(SIZE)], [1, 1, SIZE, SIZE]),
+        _int32_tensor("col_grid_i32", [c for _ in range(SIZE) for c in range(SIZE)], [1, 1, SIZE, SIZE]),
         _u8_tensor("invalid_u8", [255], [1]),
         _u8_tensor("colors10_u8", list(range(10)), [1, 10, 1, 1]),
     ]
@@ -44,18 +50,23 @@ def build_model() -> onnx.ModelProto:
         helper.make_node("ArgMax", ["row_present"], ["r_max"], axis=2, keepdims=1, select_last_index=1),
         helper.make_node("ArgMax", ["col_present"], ["c_min"], axis=3, keepdims=1),
         helper.make_node("ArgMax", ["col_present"], ["c_max"], axis=3, keepdims=1, select_last_index=1),
-        helper.make_node("Add", ["row_grid_i64", "r_min"], ["src_r"]),
-        helper.make_node("Add", ["col_grid_i64", "c_min"], ["src_c"]),
-        helper.make_node("LessOrEqual", ["src_r", "r_max"], ["row_in_crop"]),
-        helper.make_node("LessOrEqual", ["src_c", "c_max"], ["col_in_crop"]),
+        helper.make_node("Cast", ["r_min"], ["r_min_i32"], to=onnx.TensorProto.INT32),
+        helper.make_node("Cast", ["r_max"], ["r_max_i32"], to=onnx.TensorProto.INT32),
+        helper.make_node("Cast", ["c_min"], ["c_min_i32"], to=onnx.TensorProto.INT32),
+        helper.make_node("Cast", ["c_max"], ["c_max_i32"], to=onnx.TensorProto.INT32),
+        helper.make_node("Add", ["row_grid_i32", "r_min_i32"], ["src_r"]),
+        helper.make_node("Add", ["col_grid_i32", "c_min_i32"], ["src_c"]),
+        helper.make_node("LessOrEqual", ["src_r", "r_max_i32"], ["row_in_crop"]),
+        helper.make_node("LessOrEqual", ["src_c", "c_max_i32"], ["col_in_crop"]),
         helper.make_node("And", ["row_in_crop", "col_in_crop"], ["crop_valid"]),
-        helper.make_node("Where", ["crop_valid", "src_r", "zero_i64"], ["safe_r"]),
-        helper.make_node("Where", ["crop_valid", "src_c", "zero_i64"], ["safe_c"]),
-        helper.make_node("Mul", ["safe_r", "width_i64"], ["safe_r_offset"]),
+        helper.make_node("Where", ["crop_valid", "src_r", "zero_i32"], ["safe_r"]),
+        helper.make_node("Where", ["crop_valid", "src_c", "zero_i32"], ["safe_c"]),
+        helper.make_node("Mul", ["safe_r", "width_i32"], ["safe_r_offset"]),
         helper.make_node("Add", ["safe_r_offset", "safe_c"], ["safe_spatial"]),
-        helper.make_node("Reshape", ["safe_spatial", "shape_index_1x900"], ["safe_spatial_flat"]),
-        helper.make_node("Reshape", ["input_color_i64", "shape_flat_1x900"], ["color_flat_i64"]),
-        helper.make_node("Cast", ["color_flat_i64"], ["color_flat_u8"], to=onnx.TensorProto.UINT8),
+        helper.make_node("Reshape", ["safe_spatial", "shape_index_1x900"], ["safe_spatial_flat_i32"]),
+        helper.make_node("Cast", ["safe_spatial_flat_i32"], ["safe_spatial_flat"], to=onnx.TensorProto.INT64),
+        helper.make_node("Cast", ["input_color_i64"], ["input_color_u8"], to=onnx.TensorProto.UINT8),
+        helper.make_node("Reshape", ["input_color_u8", "shape_flat_1x900"], ["color_flat_u8"]),
         helper.make_node("GatherElements", ["color_flat_u8", "safe_spatial_flat"], ["gathered_flat"], axis=2),
         helper.make_node("Reshape", ["gathered_flat", "shape_1x1x30x30"], ["gathered_color"]),
         helper.make_node("Where", ["crop_valid", "gathered_color", "invalid_u8"], ["color30"]),
