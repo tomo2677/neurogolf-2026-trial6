e3b804bdbe161e9ab8498400bf4eca5f16601d0e
tomoki <tomoki.kawamoto2677@gmail.com>
2026-06-13 03:05:28 +0900

    Improve task026 solution cost

--- solutions/task026.py
+++ solutions/task026.py
@@ -11,17 +11,12 @@ def _int64_tensor(name: str, values: list[int], dims: list[int] | None = None) -
     return helper.make_tensor(name, onnx.TensorProto.INT64, shape, values)
 
 
-def _u8_tensor(name: str, values: list[int], dims: list[int]) -> onnx.TensorProto:
-    return helper.make_tensor(name, onnx.TensorProto.UINT8, dims, values)
-
-
 def _f32_tensor(name: str, values: list[float], dims: list[int]) -> onnx.TensorProto:
     return helper.make_tensor(name, onnx.TensorProto.FLOAT, dims, values)
 
 
 def build_model() -> onnx.ModelProto:
-    x, _ = make_io_value_infos()
-    y = helper.make_tensor_value_info("output", onnx.TensorProto.BOOL, GRID_SHAPE)
+    x, y = make_io_value_infos()
 
     initializers = [
         _int64_tensor("left_starts", [0, 0, 0, 0], [4]),
@@ -30,10 +25,9 @@ def build_model() -> onnx.ModelProto:
         _int64_tensor("right_ends", [1, 1, 5, 7], [4]),
         _int64_tensor("pads_output", [0, 0, 0, 0, 0, 0, 25, 27], [8]),
         _f32_tensor("zero_f32", [0.0], [1]),
-        _u8_tensor("zero_u8", [0], [1]),
-        _u8_tensor("eight_u8", [8], [1]),
-        _u8_tensor("invalid_u8", [255], [1]),
-        _u8_tensor("colors10", list(range(10)), [1, 10, 1, 1]),
+        _f32_tensor("one_f32", [1.0], [1]),
+        _f32_tensor("zero7", [0.0] * (7 * 5 * 3), [1, 7, 5, 3]),
+        _f32_tensor("zero9", [0.0] * (5 * 3), [1, 1, 5, 3]),
     ]
 
     nodes = [
@@ -42,12 +36,13 @@ def build_model() -> onnx.ModelProto:
         helper.make_node("Greater", ["left_black", "zero_f32"], ["left_is_black"]),
         helper.make_node("Greater", ["right_black", "zero_f32"], ["right_is_black"]),
         helper.make_node("And", ["left_is_black", "right_is_black"], ["both_black"]),
-        helper.make_node("Where", ["both_black", "eight_u8", "zero_u8"], ["color5x3"]),
-        helper.make_node("Pad", ["color5x3", "pads_output", "invalid_u8"], ["color30"], mode="constant"),
-        helper.make_node("Equal", ["colors10", "color30"], ["output"]),
+        helper.make_node("Cast", ["both_black"], ["eight_ch"], to=onnx.TensorProto.FLOAT),
+        helper.make_node("Sub", ["one_f32", "eight_ch"], ["zero_ch"]),
+        helper.make_node("Concat", ["zero_ch", "zero7", "eight_ch", "zero9"], ["output5x3"], axis=1),
+        helper.make_node("Pad", ["output5x3", "pads_output"], ["output"], mode="constant"),
     ]
 
-    graph = helper.make_graph(nodes, "task026_black_overlap_graph", [x], [y], initializers)
+    graph = helper.make_graph(nodes, "task026_direct_onehot_black_overlap_graph", [x], [y], initializers)
     model = helper.make_model(graph, ir_version=IR_VERSION, opset_imports=[helper.make_opsetid("", 12)])
     assert list(model.graph.output[0].type.tensor_type.shape.dim[i].dim_value for i in range(4)) == GRID_SHAPE
     return model
